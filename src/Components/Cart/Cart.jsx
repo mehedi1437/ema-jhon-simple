6c2f468dcd5be7e432fbeb8ef5cc6ea8a8310bcf
mehedi <you@example.com>
2024-02-06 01:04:09 +0600

    Cart calculation done

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const Cart = ({cart}) => {
-
-    let total = 0;
-
-    for (const product of cart){
-        total = total + product.price;
-    }
-    return (
-        <div className='bg-[#FFE0B3] p-10 space-y-5'>
-              <h4 className='font-semibold text-2xl text-center'>Order Summery</h4>
-        <p>Seleted Items : {cart.length}</p>
-        <p>Total Price : ${total}</p>
-        <p>Total Shipping Charge : $  </p>
-        <p>Tax</p>
-        <p className='font-semibold text-xl '>Grand Total : </p>
-
-        </div>
-    );
+const Cart = ({ cart }) => {
+  let totalPrice = 0;
+  let totalShipping = 0;
+  let quantity = 0;
+  console.log(cart);
+  for (const product of cart) {
+    //   if(product.quantity === 0){
+    //     product.quantity = 1;
+    //   }
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
+  }
+  const tax = (totalPrice * 7) / 100;
+  const gtandTotal = totalPrice + totalShipping + tax;
+  return (
+    <div className="bg-[#FFE0B3] p-10 space-y-5 sticky top-0">
+      <h4 className="font-semibold text-2xl text-center">Order Summery</h4>
+      <p>Seleted Items : {quantity}</p>
+      <p>Total Price : ${totalPrice.toFixed(2)}</p>
+      <p> Shipping Charge : ${totalShipping.toFixed(2)} </p>
+      <p>Tax ${tax.toFixed(2)}</p>
+      <p className="font-semibold text-xl ">
+        Grand Total :$ {gtandTotal.toFixed(2)}{" "}
+      </p>
+    </div>
+  );
 };
 
 export default Cart;
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,20 +1,58 @@
 import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-  const [cart,setCart] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    // Step-1 :get _id
+    for (const _id in storedCart) {
+      // Step-2 : get the full product by using this _id
+      const addedProduct = products.find((product) => product.id === _id);
+      // console.log(addedProduct);
+      // Step-4: get quantity of the addedProduct and update the quantity
+      if (addedProduct) {
+        const quantity = storedCart[_id];
+        addedProduct.quantity = quantity;
+        //  Step-4: save the addedProduct to the  new array savedcart
+        savedCart.push(addedProduct);
+      }
+      // console.log('added product with quantity',addedProduct);
+    }
+    // Step-5:Update the cart
+    setCart(savedCart);
+  }, [products]);
+
   const handleAddTOCart = (product) => {
-    console.log(product);
-    const newCart = [...cart,product];
+    // console.log(product);
+
+    let newCart = [];
+
+    // const newCart = [...cart, product];
+    // if product  doesn't exist ,then set quantity = 1
+    // if exist update the quantity by 1
+    const exist = cart.find((pd) => pd.id === product.id);
+    if (!exist) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exist.quantity = exist.quantity + 1;
+      const remaining= cart.filter(pd=>pd.id!==product.id);
+      newCart=[...remaining,exist];
+    }
+
     setCart(newCart);
+    addToDb(product.id);
   };
 
   return (
@@ -29,7 +67,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container bg-[#FFE0B3]">
-      <Cart cart={cart}/>
+        <Cart cart={cart} />
       </div>
     </div>
   );
